f3569c43ffb9a083823ff9606f8370ca0df56217
HenryHe <372001955@qq.com>
2017-12-05 18:09:08 +0800

    initizal draw bg image

---
+++ StrokeOrder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StrokeOrder
+//
+//  Created by Henry on 05/12/2017.
+//  Copyright © 2017 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+}
+
---
+++ StrokeOrder/WordView.swift
@@ -0,0 +1,65 @@
+//
+//  WordView.swift
+//  StrokeOrder
+//
+//  Created by Henry on 05/12/2017.
+//  Copyright © 2017 Henry. All rights reserved.
+//
+
+import UIKit
+
+class WordView: UIImageView {
+
+    lazy var bgImage: UIImage = self.createBgImage(self.bounds.size)
+    
+    private struct Constants {
+        static let bgLineColor: UIColor = .red
+        static let bgBorderLineWidth: CGFloat = 3
+        static let bgDashLineWidth: CGFloat = 1
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        self.image = bgImage
+    }
+    
+    func createBgImage(_ size: CGSize) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setStrokeColor(Constants.bgLineColor.cgColor)
+        
+        let dashPath = UIBezierPath()
+        dashPath.move(to: CGPoint(x: 0, y: 0))
+        dashPath.addLine(to: CGPoint(x: size.width, y: size.height))
+        dashPath.move(to: CGPoint(x: size.width, y: 0))
+        dashPath.addLine(to: CGPoint(x:0, y: size.height))
+        dashPath.move(to: CGPoint(x: 0, y: size.height/2))
+        dashPath.addLine(to: CGPoint(x: size.width, y: size.height/2))
+        dashPath.move(to: CGPoint(x: size.width/2, y: 0))
+        dashPath.addLine(to: CGPoint(x: size.width/2, y: size.height))
+        context.saveGState()
+        context.setLineDash(phase: 3, lengths: [3])
+        dashPath.lineWidth = Constants.bgDashLineWidth
+        dashPath.stroke()
+        context.restoreGState()
+        
+        let borderPath = UIBezierPath()
+        borderPath.move(to: CGPoint(x: Constants.bgBorderLineWidth / 2, y: Constants.bgBorderLineWidth/2))
+        borderPath.addLine(to: CGPoint(x: size.width-Constants.bgBorderLineWidth/2, y: Constants.bgBorderLineWidth/2))
+        borderPath.addLine(to: CGPoint(x: size.width-Constants.bgBorderLineWidth/2, y: size.height - Constants.bgBorderLineWidth/2))
+        borderPath.addLine(to: CGPoint(x:Constants.bgBorderLineWidth / 2, y: size.height - Constants.bgBorderLineWidth/2))
+        borderPath.close()
+        borderPath.lineWidth = Constants.bgBorderLineWidth
+        borderPath.stroke()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+
+}
